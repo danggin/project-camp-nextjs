d814207ca60a8deb85e4cd73b2356fe5200bbaee
Dain <danggin.min@gmail.com>
2024-06-10 17:52:41 +0900

    ✅ learn: Data Fetching Part.1

---
+++ data-fetching/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header';
+import Banner from '@/components/Banner';
+import MovieArea from '@/components/MovieArea';
+import Popular from '@/components/Popualar';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Popular />
+    </>
+  );
+}
