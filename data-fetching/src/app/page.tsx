413bd470194a775557f6157a7763219e88a1249f
Dain <danggin.min@gmail.com>
2024-06-11 13:03:30 +0900

    ✅ learn: Data Fetching Part.2

--- data-fetching/src/app/page.tsx
+++ data-fetching/src/app/page.tsx
@@ -1,14 +1,41 @@
-import Header from '@/components/Header';
-import Banner from '@/components/Banner';
-import MovieArea from '@/components/MovieArea';
-import Popular from '@/components/Popualar';
+import Header from "@/components/Header";
+import Banner from "@/components/Banner";
+import MovieArea from "@/components/MovieArea";
 
-export default function Home() {
+const getMovies = async (type: string) => {
+  const data = await (
+    await fetch("http://localhost:3000/api/movies?type=" + type)
+  ).json();
+  return data;
+};
+
+export default async function Home() {
+  // const popular = await getMovies("popular");
+  // const top_rated = await getMovies("top_rated");
+  // const upcoming = await getMovies("upcoming");
+  // const now_playing = await getMovies("now_playing");
+
+  const [
+    { results: popular },
+    { results: top_rated },
+    { results: upcoming },
+    { results: now_playing },
+  ] = await Promise.all([
+    getMovies("popular"),
+    getMovies("top_rated"),
+    getMovies("upcoming"),
+    getMovies("now_playing"),
+  ]);
+
+  console.log(upcoming);
   return (
     <>
       <Header />
       <Banner />
-      <Popular />
+      <MovieArea title={"POPULAR"} movies={popular} />
+      <MovieArea title={"TOP RATED"} movies={top_rated} />
+      <MovieArea title={"UPCOMING"} movies={upcoming} />
+      <MovieArea title={"NOW PLAYING"} movies={now_playing} />
     </>
   );
 }
--- data-fetching/src/components/MovieArea.tsx
+++ data-fetching/src/components/MovieArea.tsx
@@ -1,22 +1,29 @@
 import MovieCard from "./MovieCard";
 
-export default function MovieArea() {
+export type TMovie = {
+  id: string;
+  title: string;
+  poster_path: string;
+  vote_average: number;
+  release_date: string;
+};
+
+export default function MovieArea({
+  title,
+  movies,
+}: {
+  title: string;
+  movies: TMovie[];
+}) {
   return (
     <>
       <article className="bg-black py-10 px-4 xs:px-0">
         <section className="container mx-auto py-8 text-white">
           <span className="text-yellow-600">ONLINE STREAMING</span>
-          <h2 className="text-[36px] font-bold mb-8">Movie LIST</h2>
+          <h2 className="text-[36px] font-bold mb-8">{title}</h2>
           <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-0">
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
+            {movies &&
+              movies.map((movie) => <MovieCard key={movie.id} {...movie} />)}
           </div>
         </section>
       </article>
