574faa3db41c6db2d4732624a3af187b1e159cf2
Dain <danggin.min@gmail.com>
2024-06-12 17:28:03 +0900

    ✅ learn: mongoDB 적용

--- cache-example/src/components/UserList.tsx
+++ cache-example/src/components/UserList.tsx
@@ -5,27 +5,32 @@ async function fetchUsers() {
 }
 
 export default async function UserList() {
+  // const users = await fetchUsers();
   const users = await fetchUsers();
+  console.log(users);
 
   return (
     <>
       <ul className='flex flex-col gap-4'>
-        {users.map((user: { id: string; name: string; email: string }) => {
-          return (
-            <li key={user.id} className='bg-white w-[300px] p-3 rounded-lg'>
-              <div>
-                <h1>{user.name}</h1>
-                <p>{user.email}</p>
-                <div className='flex gap-4 justify-end'>
-                  <form action={deleteUser}>
-                    <input type='hidden' name='id' value={user.id} />
-                    <button className='underline text-rose-400'>Delete</button>
-                  </form>
+        {users &&
+          users.map((user: { id: string; name: string; email: string }) => {
+            return (
+              <li key={user.id} className='bg-white w-[300px] p-3 rounded-lg'>
+                <div>
+                  <h1>{user.name}</h1>
+                  <p>{user.email}</p>
+                  <div className='flex gap-4 justify-end'>
+                    <form action={deleteUser}>
+                      <input type='hidden' name='id' value={user.id} />
+                      <button className='underline text-rose-400'>
+                        Delete
+                      </button>
+                    </form>
+                  </div>
                 </div>
-              </div>
-            </li>
-          );
-        })}
+              </li>
+            );
+          })}
       </ul>
     </>
   );
