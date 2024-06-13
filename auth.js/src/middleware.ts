7b1d36dd33d250712cd6d20034df6d44480e7563
Dain <danggin.min@gmail.com>
2024-06-13 16:52:57 +0900

    🐛 fix: middleware.ts 수정

--- auth.js/src/middleware.ts
+++ auth.js/src/middleware.ts
@@ -1,1 +1,7 @@
-export { auth as middleware } from "@/auth";
+import { NextRequest, NextResponse } from 'next/server';
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
+export async function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
