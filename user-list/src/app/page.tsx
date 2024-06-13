02547241d9a984d9ee81626271cc1a48969aa7c3
Dain <danggin.min@gmail.com>
2024-06-13 12:00:24 +0900

    💄 styles: 정렬 수정

--- user-list/src/app/page.tsx
+++ user-list/src/app/page.tsx
@@ -5,7 +5,7 @@ import UserList from '@/components/UserList';
 export default function page() {
   return (
     <>
-      <div className='flex flex-col justify-center items-center h-screen gap-8'>
+      <div className='flex flex-col justify-top p-5 items-center h-screen gap-8'>
         <FormAdd />
         <UserList />
       </div>
